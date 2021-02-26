5679fcc51500a989f39f956178828437bbca4498
Jorge Salgado <jaso_98@hotmail.com>
2021-02-26 13:03:55 -0600

    FIX: allowed_hosts

--- codingnews/settings.py
+++ codingnews/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['codinghacknews.herokuapp.com', '*']
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'frontpage'
